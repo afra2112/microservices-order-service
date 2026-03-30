f243c0bce4a429d6d81bb901a1bed77150dd6c6a
Andres  Felipe Ramirez  Agudelo <andres.ramirez01@globant.com>
2026-03-29 21:23:00 -0500

    fixed created and updated at

--- src/main/java/org/microservice/orderservice/service/order/OrderMapper.java
+++ src/main/java/org/microservice/orderservice/service/order/OrderMapper.java
@@ -5,6 +5,8 @@ import org.microservice.orderservice.controller.dto.OrderResponse;
 import org.microservice.orderservice.entity.Order;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class OrderMapper {
 
@@ -15,6 +17,8 @@ public class OrderMapper {
                 .reference(request.reference())
                 .totalAmount(request.amount())
                 .paymentMethod(request.paymentMethod())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .build();
     }
 
